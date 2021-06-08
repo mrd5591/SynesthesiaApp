f7b52440b0e6d6453ecf23f28e37d90d47ed90a4
mrd5591 <mrd5591@psu.edu>
2021-06-08 13:07:31 -0400

    Local email

--- Synesthesia/ResultsPage.xaml.cs
+++ Synesthesia/ResultsPage.xaml.cs
@@ -7,6 +7,12 @@ using Newtonsoft.Json;
 using Synesthesia.Models;
 using Rg.Plugins.Popup.Services;
 using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
+using System.IO;
+using System.Text;
+using CsvHelper;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace Synesthesia
 {
@@ -19,6 +25,8 @@ namespace Synesthesia
             InitializeComponent();
 
             this.colorMapping = colorMapping;
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
         }
 
         public async void Button_Clicked(System.Object sender, System.EventArgs e)
@@ -27,17 +35,22 @@ namespace Synesthesia
 
             string fileType;
 
-            if(selection.Equals("CSV"))
+            if (selection.Equals("CSV"))
             {
-                fileType = "csv";
-            } else
+                //fileType = "csv";
+                CreateCSV();
+            }
+            else
             {
-                fileType = "xlsx";
+                //fileType = "xlsx";
+                CreateXLSX();
             }
 
             string email = EmailEntry.Text;
 
-            SubmitJsonPackage pkg = new SubmitJsonPackage();
+            await SendEmail(email);
+
+            /*SubmitJsonPackage pkg = new SubmitJsonPackage();
             pkg.email = email;
             pkg.fileType = fileType;
 
@@ -59,7 +72,7 @@ namespace Synesthesia
 
             await PopupNavigation.Instance.PushAsync(new PopupPage());
             String result = RESTClient.Post(new Uri(App.url + "submit"), JsonConvert.SerializeObject(pkg));
-            /*await PopupNavigation.Instance.PopAsync();
+            await PopupNavigation.Instance.PopAsync();
 
             JObject rss = JObject.Parse(result);
 
@@ -137,6 +150,91 @@ namespace Synesthesia
             else
             {
                 ResultButton.IsEnabled = false;
+            }
+        }
+
+        private void CreateCSV()
+        {
+            tempFile = Path.GetTempFileName() + ".csv";
+            var records = new List<string[]>
+            {
+                new string[] {"Stimulus", "Red", "Green", "Blue", "Alpha", "Hue", "Luminosity"}
+            };
+
+            foreach(KeyValuePair<string, Color> pair in colorMapping)
+            {
+                Color c = pair.Value;
+                records.Add(new string[] {pair.Key, c.R.ToString(), c.G.ToString(), c.B.ToString(), c.A.ToString(), c.Hue.ToString(), c.Luminosity.ToString() });
+            }
+
+            using (var writer = new StreamWriter(tempFile))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+
+        private void CreateXLSX()
+        {
+            tempFile = Path.GetTempFileName() + ".xlsx";
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                excelPackage.Workbook.Properties.Title = "SynesthesiaResults";
+                excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+
+                worksheet.Cells["A1"].Value = "Stimuls";
+                worksheet.Cells["A2"].Value = "Red";
+                worksheet.Cells["A3"].Value = "Green";
+                worksheet.Cells["A4"].Value = "Blue";
+                worksheet.Cells["A5"].Value = "Alpha";
+                worksheet.Cells["A6"].Value = "Hue";
+                worksheet.Cells["A7"].Value = "Luminosity";
+
+                int row = 2;
+                foreach (KeyValuePair<string, Color> pair in colorMapping)
+                {
+                    int col = 1;
+                    Color c = pair.Value;
+                    worksheet.Cells[col, row].Value = pair.Key;
+                    worksheet.Cells[++col, row].Value = c.R;
+                    worksheet.Cells[++col, row].Value = c.G;
+                    worksheet.Cells[++col, row].Value = c.B;
+                    worksheet.Cells[++col, row].Value = c.A;
+                    worksheet.Cells[++col, row].Value = c.Hue;
+                    worksheet.Cells[++col, row].Value = c.Luminosity;
+
+                    row++;
+                }
+
+                FileInfo fi = new FileInfo(tempFile);
+                excelPackage.SaveAs(fi);
+            }
+        }
+
+        public async Task SendEmail(string email)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = "Your Synesthesia Data!",
+                    Body = "The requested file type is attached.",
+                    To = new List<string> {email },
+                };
+
+                message.Attachments.Add(new EmailAttachment(tempFile));
+
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                Console.WriteLine(fbsEx);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
         }
     }
